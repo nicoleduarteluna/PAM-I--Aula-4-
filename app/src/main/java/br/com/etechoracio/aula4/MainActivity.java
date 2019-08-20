8736b6266d3e0180be7335ea94debac32fa6d1b0
niluiz <nicoleduarteluna@hotmail.com>
2019-08-19 21:56:40 -0300

    Aula 4 - 19_08_2019

---
+++ app/src/main/java/br/com/etechoracio/aula4/MainActivity.java
@@ -0,0 +1,17 @@
+package br.com.etechoracio.aula4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void onCalcular (View view) {
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
